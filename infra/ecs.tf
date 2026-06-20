1d21127bbfb0b1e8b461846cb9bf0ebc99bf7b3b
Lavanya-Mtl <lavanyamatlani1@gmail.com>
2026-06-20 18:58:56 +0530

    added python healthcheck

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -49,11 +49,11 @@ resource "aws_ecs_task_definition" "app" {
     }]
 
     healthCheck = {
-      command     = ["CMD-SHELL", "curl -f http://localhost:${var.container_port}/health || exit 1"]
-      interval    = 30
-      timeout     = 5
-      retries     = 3
-      startPeriod = 10
+    command     = ["CMD-SHELL", "python3 -c \"import urllib.request; urllib.request.urlopen('http://localhost:${var.container_port}/health')\" || exit 1"]
+    interval    = 30
+    timeout     = 10
+    retries     = 3
+    startPeriod = 60
     }
 
     logConfiguration = {
